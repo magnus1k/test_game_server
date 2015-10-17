c65ad7f45e19586f5e99f694f9efd4ce21b5e5af
Magnus <1k.magnus@gmail.com>
2015-10-18 01:05:05 +0800

    change format

--- tool/clienttest.py
+++ tool/clienttest.py
@@ -1,45 +1,48 @@
-#coding:utf8
+# coding:utf8
 
 import time
 
-from socket import AF_INET,SOCK_STREAM,socket
+from socket import AF_INET, SOCK_STREAM, socket
 from thread import start_new
 import struct
-HOST='localhost'
-PORT=1000
-BUFSIZE=1024
-ADDR=(HOST , PORT)
-client = socket(AF_INET,SOCK_STREAM)
+HOST = 'localhost'
+PORT = 1000
+BUFSIZE = 1024
+ADDR = (HOST, PORT)
+client = socket(AF_INET, SOCK_STREAM)
 client.connect(ADDR)
 
-def sendData(sendstr,commandId):
-    HEAD_0 = chr(0)
-    HEAD_1 = chr(0)
-    HEAD_2 = chr(0)
-    HEAD_3 = chr(0)
-    ProtoVersion = chr(0)
-    ServerVersion = 0
+
+def send_data(sendstr, command_id):
+    head_0 = chr(0)
+    head_1 = chr(0)
+    head_2 = chr(0)
+    head_3 = chr(0)
+    proto_version = chr(0)
+    server_version = 0
     sendstr = sendstr
-    data = struct.pack('!sssss3I',HEAD_0,HEAD_1,HEAD_2,\
-                       HEAD_3,ProtoVersion,ServerVersion,\
-                       len(sendstr)+4,commandId)
+    data = struct.pack('!sssss3I', head_0, head_1, head_2,
+                       head_3, proto_version, server_version,
+                       len(sendstr)+4, command_id)
     senddata = data+sendstr
     return senddata
 
-def resolveRecvdata(data):
-    head = struct.unpack('!sssss3I',data[:17])
+
+def resolve_recvdata(data):
+    head = struct.unpack('!sssss3I', data[:17])
     length = head[6]
     data = data[17:17+length]
     return data
 
 s1 = time.time()
 
+
 def start():
-    for i in xrange(10):
-        client.sendall(sendData('asdfe',1))
+    for num in xrange(10):
+        client.sendall(send_data('asdfe', 1))
 
 for i in range(10):
-    start_new(start,())
+    start_new(start, ())
 while True:
     pass
 
