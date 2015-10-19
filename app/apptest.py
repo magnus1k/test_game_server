17c43ff19dd621020627bb8ea7a32a358f305303
Magnus <1k.magnus@gmail.com>
2015-10-19 16:14:44 +0800

    测试协议

--- app/apptest.py
+++ app/apptest.py
@@ -5,9 +5,5 @@ from firefly.server.globalobject import netserviceHandle
 
 @netserviceHandle
 def echo_1(_conn, data):
-    _conn == _conn
+    print (data)
     return data
-
-    
-
-
--- appmain.py
+++ appmain.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     config = None
     if len(args) > 2:
         servername = args[1]
-        config = json.load(open(args[2],'r'))
+        config = json.load(open(args[2], 'r'))
     else:
         raise ValueError
     dbconf = config.get('db')
